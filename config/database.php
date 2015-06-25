83028d941fcccbe90d93b0736544969f05c56a1a
Clark Winkelmann <clark.winkelmann@gmail.com>
2015-06-25 09:24:04 +0200

    Séparé la config du fichier principal

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Paramètres de base de donnée
+ * Sauvegarder dans database.php
+ */
+define('DB_SERVER',   'localhost');
+define('DB_USERNAME', 'robotsju');
+define('DB_PASSWORD', 'robotsju');
+define('DB_DATABASE', 'robotsju');
---
+++ config/site.example.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Importation de la configuration
+ * Elle est séparée du fichier principal pour permettre à Concrete
+ * de faire des éditions ici sans gêner les mises à jour futures
+ */
+require('robots-ju.php');
